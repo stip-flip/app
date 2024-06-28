2e8ce722dbacc3763ac7f6d70f8d9f2cfc7824a5
sotach1 <sotachi@proton.me>
2024-06-28 14:36:18 +0200

    no more countdown

--- src/components/countdown.svelte
+++ src/components/countdown.svelte
@@ -6,40 +6,40 @@
   $: time = getHoursMinutesSeconds(1718755200, $timestamp);
 </script>
 
-{#if $timestamp > settlement}
-  <div class="w-screen z-10">
-    <div class="w-1/2 m-auto">
-      <div class="py-2 flex space-x-4">
-        <a
-          class="text-center font-thin py-12 text-xl fine-border rounded-tl-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:text-primary hover:font-normal"
-          href="/swap"
-        >
-          Trade
-        </a>
-        <a
-          class="text-center font-thin py-12 text-xl fine-border rounded-tr-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:text-primary hover:font-normal"
-          href="/earn"
-        >
-          Deposit
-        </a>
-      </div>
-      <div class="py-2 flex space-x-4">
-        <a
-          class="text-center font-thin py-12 text-xl fine-border rounded-bl-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:text-primary hover:font-normal"
-          href="/oracle/deposit"
-        >
-          Stake
-        </a>
-        <a
-          class="text-center font-thin py-12 text-xl fine-border rounded-br-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:text-primary hover:font-normal"
-          href="https://docs.sf.exchange"
-        >
-          Learn
-        </a>
-      </div>
+<!-- {#if $timestamp > settlement} -->
+<div class="w-screen z-10">
+  <div class="w-1/2 m-auto">
+    <div class="py-2 flex space-x-4">
+      <a
+        class="text-center font-thin py-12 text-xl fine-border rounded-tl-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:border-primary hover:font-normal"
+        href="/swap"
+      >
+        Trade
+      </a>
+      <a
+        class="text-center font-thin py-12 text-xl fine-border rounded-tr-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:border-primary hover:font-normal"
+        href="/earn"
+      >
+        Deposit
+      </a>
+    </div>
+    <div class="py-2 flex space-x-4">
+      <a
+        class="text-center font-thin py-12 text-xl fine-border rounded-bl-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:border-primary hover:font-normal"
+        href="/oracle/deposit"
+      >
+        Stake
+      </a>
+      <a
+        class="text-center font-thin py-12 text-xl fine-border rounded-br-full w-1/2 hover:bg-white hover:bg-opacity-20 hover:border-primary hover:font-normal"
+        href="https://docs.sf.exchange"
+      >
+        Learn
+      </a>
     </div>
   </div>
-{:else}
+</div>
+<!-- {:else}
   <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
     <div
       class="flex flex-col p-2 bg-neutral bg-opacity-50 rounded-box text-neutral-content"
@@ -74,4 +74,4 @@
       sec
     </div>
   </div>
-{/if}
+{/if} -->
