7364d2de2131bbe5e1f1a6c8d5a7d624b3f2c49c
sotach1 <sotachi@proton.me>
2024-07-18 13:51:47 +0200

    pwa test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import { VitePWA } from "vite-plugin-pwa";
+
 import { defineConfig } from "vite";
 
 const generateSW = true;
@@ -7,18 +8,6 @@ const generateSW = true;
 export default defineConfig({
   // WARN: this will not be necessary on your project
   logLevel: "info",
-  // WARN: this will not be necessary on your project
-  build: {
-    minify: false,
-  },
-  // WARN: this will not be necessary on your project
-  define: {
-    __DATE__: `'${new Date().toISOString()}'`,
-    __RELOAD_SW__: false,
-    "process.env.NODE_ENV":
-      process.env.NODE_ENV === "production" ? '"production"' : '"development"',
-  },
-  // WARN: this will not be necessary on your project
   server: {
     fs: {
       // Allow serving files from hoisted root node_modules
@@ -27,43 +16,10 @@ export default defineConfig({
   },
   plugins: [
     sveltekit(),
-    SvelteKitPWA({
-      srcDir: "./src",
-      mode: "development",
-      // you don't need to do this if you're using generateSW strategy in your app
-      strategies: generateSW ? "generateSW" : "injectManifest",
-      // you don't need to do this if you're using generateSW strategy in your app
-      filename: generateSW ? undefined : "prompt-sw.ts",
-      scope: "/",
-      base: "/",
-      selfDestroying: process.env.SELF_DESTROYING_SW === "true",
-      pwaAssets: {
-        config: true,
-      },
-      manifest: {
-        short_name: "SvelteKit PWA",
-        name: "SvelteKit PWA",
-        start_url: "/",
-        scope: "/",
-        display: "standalone",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-      },
-      injectManifest: {
-        globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
-      },
-      workbox: {
-        globPatterns: ["client/**/*.{js,css,ico,png,svg,webp,woff,woff2}"],
-      },
+    VitePWA({
+      registerType: "autoUpdate",
       devOptions: {
-        enabled: false,
-        suppressWarnings: process.env.SUPPRESS_WARNING === "true",
-        type: "module",
-        navigateFallback: "/",
-      },
-      // if you have shared info in svelte config file put in a separate module and use it also here
-      kit: {
-        includeVersionFile: true,
+        enabled: true,
       },
     }),
   ],
