eb9eb8795d9190f4fa8dd505f1095a5b212b2f59
sotach1 <sotachi@proton.me>
2024-07-19 17:41:08 +0200

    pwa build

--- vite.config.ts
+++ vite.config.ts
@@ -31,18 +31,6 @@ export default defineConfig({
         start_url: "/",
         display: "fullscreen",
         background_color: "#1b006ae6",
-        // icons: [
-        //   {
-        //     src: "/icons/icon-192x192.png",
-        //     sizes: "192x192",
-        //     type: "image/png",
-        //   },
-        //   {
-        //     src: "/icons/icon-512x512.png",
-        //     sizes: "512x512",
-        //     type: "image/png",
-        //   },
-        // ],
       },
       registerType: "autoUpdate",
       workbox: {
