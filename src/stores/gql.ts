50af43f374d6d735f62217b2f0c97aa7f4492215
sotach1 <sotachi@proton.me>
2026-04-24 15:19:14 +0200

    Update GraphQL endpoints to stipflip.xyz

--- src/stores/gql.ts
+++ src/stores/gql.ts
@@ -4,28 +4,28 @@ import { derived, type Readable } from "svelte/store";
 import { chainId, signer } from "svelte-ethers-store";
 
 export const gqlsdk: Readable<Sdk> = derived(
-  [signer, chainId],
-  ([$signer, $chainId], set) => {
-    switch ($chainId) {
-      case 61:
-        const etc = new GraphQLClient(
-          "https://ether-graphiql.sf.exchange/subgraphs/name/sotachi/sf"
-        );
-        set(getSdk(etc));
-        break;
-      case 63:
-        const mordor = new GraphQLClient(
-          "https://mordor-graphiql.sf.exchange/subgraphs/name/sotachi/sf"
-        );
-        set(getSdk(mordor));
-        break;
-      default:
-        const def = new GraphQLClient(
-          "https://ether-graphiql.sf.exchange/subgraphs/name/sotachi/sf"
-        );
-        set(getSdk(def));
-        break;
-        break;
-    }
-  }
+    [signer, chainId],
+    ([$signer, $chainId], set) => {
+        switch ($chainId) {
+            case 61:
+                const etc = new GraphQLClient(
+                    "https://ether-graphiql.stipflip.xyz/subgraphs/name/sotachi/sf",
+                );
+                set(getSdk(etc));
+                break;
+            case 63:
+                const mordor = new GraphQLClient(
+                    "https://mordor-graphiql.stipflip.xyz/subgraphs/name/sotachi/sf",
+                );
+                set(getSdk(mordor));
+                break;
+            default:
+                const def = new GraphQLClient(
+                    "https://ether-graphiql.stipflip.xyz/subgraphs/name/sotachi/sf",
+                );
+                set(getSdk(def));
+                break;
+                break;
+        }
+    },
 );
